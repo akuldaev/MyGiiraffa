58a723e7ad6a3bbec24e378e65e92daeb1f0e3c9
akuldaev <alexkuldaev@gmail.com>
2019-03-02 23:58:04 +0300

    fix url in ionic

--- build/108.js
+++ build/108.js
@@ -1,6 +1,6 @@
 webpackJsonp([108],{
 
-/***/ 1845:
+/***/ 1847:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- build/115.js
+++ build/115.js
@@ -1,6 +1,6 @@
 webpackJsonp([115],{
 
-/***/ 1837:
+/***/ 1835:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
